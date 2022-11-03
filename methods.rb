aff1ae0a8043618654738a2bfaa7f719f19656a9
May Chen <maymchen1@gmail.com>
2022-11-03 15:51:55 -0400

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greetProgrammer() 
+  puts"Hello, programmer!"
+end
+
+def greet()
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts"Hello, #{name}!"
+end
+
+def add (num1, num2)
+  return num1 + num2
+end
+
+def halve(number) 
+  if (typeof number !== "number") 
+    return null;
+    return number / 2;
+  end
+
